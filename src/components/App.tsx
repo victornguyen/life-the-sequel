d5382a018935718df2bca8f5d6339554470f9c3f
Victor Nguyen <vic.nguyen@marketplacer.com>
2021-08-21 11:36:37 +1000

    Organise components into components directory
    Also remove unneeded scaffolding

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1>The Game of Life II</h1>
+    </div>
+  );
+}
+
+export default App;
